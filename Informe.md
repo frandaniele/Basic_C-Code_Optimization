4dfe72f9b884b6099948367818616ef4e6866ef9
frandaniele <francisco.daniele@mi.unc.edu.ar>
2022-04-12 17:10:22 -0300

    pruebas fallidas

--- Informe.md
+++ Informe.md
@@ -6,4 +6,15 @@
 5. cambie %d por %i en los prints
 6. hice loop unrolling de for de la primera l desp de for de k en compute
 7. ver analysis 1-8
-8. saque todos los + 1 en tmp_sum (9) e inicie accum en 9
+8. saque la multiplicacion por 4 en los for y la pase directamente a c/ valor de kern
+9. saque todos los + 1 en tmp_sum (9) e inicie accum en 9 (analysis 9)
+10. memset analysis 10
+11. elimine las mult que daban 0 y por consiguiente la suma de esos resultados (analysis 11)
+12. hasta nalysis 14 no hay mas cambios que los hasta aca hechos
+
+# ver
+arr[x][9998] = arr[x][9999] siempre -- esta
+arr[0][x] = 0 siempre -- esta
+
+arr[9999][x] = 9.539862 siempre
+arr[x][9999] = arr[x+1][0] siempre => arr[x][9998] = arr[x][9999] = arr[x+1][0] 
--- lab3.c
+++ lab3.c
@@ -14,20 +14,15 @@ double **alloc_matrix(void) /* Allocate the array */{
     double **array;
     array = malloc(XDIM*sizeof(double *));
 
-    //size_t size_to_alloc = YDIM*sizeof(double); //para calcularlo una sola vez
     for(i = 0; i < XDIM; i++)
         array[i] = malloc(YDIM*sizeof(double));
   
-   /* for(j=0; j<XDIM; j++)
-        for(k=0; k<YDIM; k++)
-            memset(&array[k][j], j, sizeof(double));*/
     return array;
 }
 
 void fill(double** arr){
     int i, j;
-    //time_t t1; 
-    //srand ( (unsigned) time (&t1));
+
     for(i = 0; i < XDIM; i++)
         for(j = 0; j < YDIM; j++)
             arr[i][j] = (double)(rand() % 100);
@@ -36,89 +31,46 @@ void fill(double** arr){
 void compute(double** arr, int kern[3][3]){
     double tmp_sum[9];
     double dato, accum;
-    int i, j;//, k;//, l;
+    int i, j;
+
     for(i = 0; i < XDIM; i++){
         int x = i - 1;
+
         for(j = 0; j < YDIM; j++){
             //printf("processing: %i - %i \n", i, j); //lo hace muuuuy lento
             if(i >= 1 && j >=1 && i < XDIM-1 && j <YDIM-1){
-                /*for(k = 0; k < 3; k++){
-                   // for(l = 0; l < 3; l++){
-                        //int x = i + (l-1);
-                        //int y = j + (k-1);// no se usa esta variable,
-                       // tmp_sum[l*3+k] = 2*(2*kern[l][k]*dato)/1000 + 1;//capaz un shift en lugar de mult
-                        dato = arr[x][j];
-                        tmp_sum[k] = 2*(2*kern[0][k]*dato)/1000 + 1;
-
-                        dato = arr[x + 1][j];
-                        tmp_sum[3 + k] = 2*(2*kern[1][k]*dato)/1000 + 1;
-                        
-                        dato = arr[x + 2][j];
-                        tmp_sum[6 + k] = 2*(2*kern[2][k]*dato)/1000 + 1;
-                   // }
-                }*/
-
-               /* dato = arr[x][j];
-                tmp_sum[0] = (4*kern[0][0]*dato)/1000 + 1;
-                tmp_sum[1] = (4*kern[0][1]*dato)/1000 + 1;
-                tmp_sum[2] = (4*kern[0][2]*dato)/1000 + 1;
-
-                dato = arr[x + 1][j];
-                tmp_sum[3] = (4*kern[1][0]*dato)/1000 + 1;
-                tmp_sum[4] = (4*kern[1][1]*dato)/1000 + 1;
-                tmp_sum[5] = (4*kern[1][2]*dato)/1000 + 1;
-                        
-                dato = arr[x + 2][j];
-                tmp_sum[6] = (4*kern[2][0]*dato)/1000 + 1;
-                tmp_sum[7] = (4*kern[2][1]*dato)/1000 + 1;
-                tmp_sum[8] = (4*kern[2][2]*dato)/1000 + 1;*/
-
                 dato = arr[x][j];
-                tmp_sum[0] = (kern[0][0]*dato)/1000;// + 1;
-                tmp_sum[1] = (kern[0][1]*dato)/1000;// + 1;
-                tmp_sum[2] = (kern[0][2]*dato)/1000;// + 1;
+                tmp_sum[1] = (kern[0][1]*dato)/1000;
 
                 dato = arr[x + 1][j];
-                tmp_sum[3] = (kern[1][0]*dato)/1000;// + 1;
-                tmp_sum[4] = (kern[1][1]*dato)/1000;// + 1;
-                tmp_sum[5] = (kern[1][2]*dato)/1000;// + 1;
-                        
+                tmp_sum[3] = (kern[1][0]*dato)/1000;
+                tmp_sum[4] = (kern[1][1]*dato)/1000;
+                tmp_sum[5] = (kern[1][2]*dato)/1000;
+                            
                 dato = arr[x + 2][j];
-                tmp_sum[6] = (kern[2][0]*dato)/1000;// + 1;
-                tmp_sum[7] = (kern[2][1]*dato)/1000;// + 1;
-                tmp_sum[8] = (kern[2][2]*dato)/1000;// + 1;
+                tmp_sum[7] = (kern[2][1]*dato)/1000;
 
-                //accum = 0;
                 accum = 9;
-               /* for(k = 0; k < 3; k++){
-                   // for(l = 0; l < 3; l++)
-                       // accum = accum + tmp_sum[k*3+l];
-                    double tmp = tmp_sum[k*3] + tmp_sum[k*3 + 1] + tmp_sum[k*3 + 2];
-                    accum = accum + tmp;
-                }*/
-                double tmp = tmp_sum[0] + tmp_sum[1] + tmp_sum[2];
-                accum = accum + tmp;
-                tmp = tmp_sum[3] + tmp_sum[4] + tmp_sum[5];
-                accum = accum + tmp;
-                tmp = tmp_sum[6] + tmp_sum[7] + tmp_sum[8];
+                
+                double tmp = tmp_sum[1] + tmp_sum[3] + tmp_sum[4] + tmp_sum[5] + tmp_sum[7];
                 accum = accum + tmp;
             }
             arr[i][j] = accum;
         }
-    }    
+    }
 }
 
 void print(double** arr){
     int i, j;
-    for(i = 0 ; i < 3; i++)
+    for(i = 0 ; i < 3 ; i++)
         for(j = 0 ; j < 5; j++)
             printf("array[%i][%i] = %f\n", i, j, arr[i][j]);
+            //printf("array[%i][%i] = %f\narray[%i][%i] = %f\narray[%i][%i] = %f\narray[%i][%i] = %f\narray[%i][%i] = %f\n", i, 0, arr[i][0], i, 1, arr[i][1], i, 2, arr[i][2], i, 3, arr[i][3], i, 4, arr[i][4]);
+
 }
 
 int main(void){
-   // int i = 0;
     double **arr;
- //   int kern[3][3] = {{0, -1, 0}, {-1, 5, -1}, {0, -1, 0}};
     int kern[3][3] = {{0, -4, 0}, {-4, 20, -4}, {0, -4, 0}};
 
     arr = alloc_matrix();
